3fe8cbe4c2671ef1bd6dafcf70ecc2c885ea2bb5
kogcyc <kogcyc@gmail.com>
2024-01-17 08:15:56 -0800

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,10 +1,13 @@
 import flask
+import os
 
 app = flask.Flask(__name__)
 
+ss = os.getenv('SUPER_SECRET')
+
 @app.route('/')
 def index():
-    return "Hello World!"
+    return f'Hello World!, my secret is {ss}'
 
 if __name__ == '__main__':
     app.run()
